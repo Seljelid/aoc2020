f98914a06f3011cf8c967ac77bfc823390ba5837
Seljelid <lars.eirikseljelid@gmail.com>
2020-12-02 08:43:23 +0100

    Solution to day 2

---
+++ src/20201202.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def validate_passwords():
+    df = pd.read_csv(
+        "data/20201202.txt",
+        names=["low", "high", "letter", "pw"],
+        sep="-| ",
+        engine="python",
+    )
+    df.letter = df.letter.str.replace(r"\:", "")
+
+    # Part 1
+    # df["valid"] = df.apply(
+    #     lambda x: (x.pw.count(x.letter) >= x.low) & (x.pw.count(x.letter) <= x.high),
+    #     axis=1,
+    # )
+    # print(df.valid.value_counts())
+
+    # Part 2
+    df["valid"] = df.apply(
+        lambda x: (x.pw.count(x.letter, x.low - 1, x.low) == 1)
+        ^ (x.pw.count(x.letter, x.high - 1, x.high) == 1),
+        axis=1,
+    )
+
+
+if __name__ == "__main__":
+    validate_passwords()
