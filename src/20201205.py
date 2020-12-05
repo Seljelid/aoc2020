ea27f7aa05587df97539ef82a6fa1409980b0665
Seljelid <lars.eirikseljelid@gmail.com>
2020-12-05 11:10:20 +0100

    A little bit cleaner

--- src/20201205.py
+++ src/20201205.py
@@ -7,22 +7,13 @@ def boarding_passes():
     rows = []
     columns = []
     for line in lines:
-        rows.append(line[0:7])
-        columns.append(line[7:-1])
-
-    row_nums = []
-    for row in rows:
-        row_num = _get_row_num(row)
-        row_nums.append(row_num)
-
-    col_nums = []
-    for col in columns:
-        col_num = _get_col_num(col)
-        col_nums.append(col_num)
+        row, col = _get_row_and_col(line)
+        rows.append(row)
+        columns.append(col)
 
     bdp = pd.DataFrame()
-    bdp["row"] = row_nums
-    bdp["col"] = col_nums
+    bdp["row"] = rows
+    bdp["col"] = columns
     bdp["seat_id"] = bdp.row * 8 + bdp.col
     print(f"Highest seat id is: {bdp.seat_id.max()}")
 
@@ -40,24 +31,19 @@ def boarding_passes():
     print(f"My seat id is: {my_seat_id}")
 
 
-def _get_row_num(code: str):
+def _get_row_and_col(code: str):
     row_range = list(range(0, 128))
+    col_range = list(range(0, 8))
     for char in code:
         if char == "F":
             row_range = row_range[: len(row_range) // 2]
-        else:
+        elif char == "B":
             row_range = row_range[len(row_range) // 2 :]
-    return row_range[0]
-
-
-def _get_col_num(code: str):
-    col_range = list(range(0, 8))
-    for char in code:
-        if char == "L":
+        elif char == "L":
             col_range = col_range[: len(col_range) // 2]
         else:
             col_range = col_range[len(col_range) // 2 :]
-    return col_range[0]
+    return row_range[0], col_range[0]
 
 
 if __name__ == "__main__":
