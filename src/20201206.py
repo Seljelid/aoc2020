41e2625c6537e09753c32b65e6c22916115bec6d
Seljelid <lars.eirikseljelid@gmail.com>
2020-12-06 10:05:19 +0100

    Solution to part 2

--- src/20201206.py
+++ src/20201206.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 def customs_control():
     file = open("data/20201206.txt")
     lines = file.readlines()
@@ -15,9 +12,24 @@ def customs_control():
         if idx == len(lines) - 1:
             groups.append(group)
 
-    # common_elements = [list(set.intersection(*map(set, grp))) for grp in groups]
     common_count = [len(group) for group in groups]
-    print(f"The sum of common answers in: {sum(common_count)}")
+    print(f"The sum of common answers in: {sum(common_count)} in part 1")
+
+    # Part 2
+    groups = []
+    group = []
+    for idx, line in enumerate(lines):
+        if not line.isspace():
+            group.append(list(list(line[:-1])))
+        else:
+            groups.append(group)
+            group = []
+        if idx == len(lines) - 1:
+            groups.append(group)
+
+    common_elements = [list(set.intersection(*map(set, grp))) for grp in groups]
+    common_count = [len(common) for common in common_elements]
+    print(f"The sum of common answers in: {sum(common_count)} in part 2")
 
 
 if __name__ == "__main__":
