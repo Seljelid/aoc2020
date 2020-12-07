db0072b78bad0fe1d45a9639851bbb0dcc75643d
Seljelid <lars.eirikseljelid@gmail.com>
2020-12-07 23:48:00 +0100

    Less type casting

--- src/20201207.py
+++ src/20201207.py
@@ -22,14 +22,14 @@ def bags():
 
     def _extract_bags(bag: dict, current_collection: dict):
         next_level = [Counter(bags[k]) for k in bag.keys()]
-        cnt = Counter()
+        next_bag = Counter()
         for elem, multiplier in zip(next_level, bag.values()):
             for k in elem.keys():
                 elem[k] = elem[k] * multiplier
-            cnt += elem
-        next_bag = dict(cnt)
+            next_bag += elem
+
         if bool(next_bag):
-            current_collection = dict(Counter(current_collection) + Counter(next_bag))
+            current_collection = dict(Counter(current_collection) + next_bag)
             return _extract_bags(next_bag, current_collection)
         else:
             return current_collection
@@ -40,8 +40,8 @@ def bags():
         bag_dict[outer_bag] = inner_bags
 
     counter = 0
-    for k, v in bag_dict.items():
-        if "shiny gold" in v.keys():
+    for contents in bag_dict.values():
+        if "shiny gold" in contents:
             counter += 1
     print(f"There can be shiny golden bags in {counter} bag types")
 
